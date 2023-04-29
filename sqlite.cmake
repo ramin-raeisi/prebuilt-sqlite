99328f3f6a13f130a29709bd7a8afec07cd2f0e0
Ramin Raeisi <rr@epen.io>
2023-04-29 08:53:27 +0200

    Add version 3.7.16.2

---
+++ sqlite.cmake
@@ -0,0 +1,13 @@
+find_path(SQLITE_INCLUDE CppSQLite3.h PATHS ${CMAKE_CURRENT_LIST_DIR})
+
+find_path(SQLITE_LIB_RELEASE CppSQLite3.lib PATHS ${CMAKE_CURRENT_LIST_DIR}/cmake64/release)
+find_path(SQLITE_LIB_DEBUG CppSQLite3.lib PATHS ${CMAKE_CURRENT_LIST_DIR}/cmake64/debug)
+set(_suffix vs110_64)
+
+include_directories(${SQLITE_INCLUDE})
+
+if(${AZI_STATIC_LIBS})
+  set(Sqlite_LIBRARIES debug ${SQLITE_LIB_DEBUG}/sqlite3_static.lib debug ${SQLITE_LIB_DEBUG}/CppSQLite3.lib optimized ${SQLITE_LIB_RELEASE}/sqlite3_static.lib optimized ${SQLITE_LIB_RELEASE}/CppSQLite3.lib)
+else(${AZI_STATIC_LIBS})
+  set(Sqlite_LIBRARIES debug ${SQLITE_LIB_DEBUG}/sqlite3d_${_suffix}.lib debug ${SQLITE_LIB_DEBUG}/CppSQLite3.lib optimized ${SQLITE_LIB_RELEASE}/sqlite3${_suffix}.lib optimized ${SQLITE_LIB_RELEASE}/CppSQLite3.lib)
+endif(${AZI_STATIC_LIBS})
